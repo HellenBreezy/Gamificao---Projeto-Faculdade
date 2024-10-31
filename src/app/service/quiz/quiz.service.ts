cb50c290fc37d5fcc152eb42b8e4515e1b159e07
Hellen Rodrigues <vivir559@gmail.com>
2024-10-31 18:25:21 -0300

    adicionando perguntas

--- src/app/service/quiz/quiz.service.ts
+++ src/app/service/quiz/quiz.service.ts
@@ -13,16 +13,64 @@ interface Question {
 export class QuizService {
   private questions: Question[] = [
     {
-      question: 'Qual é o símbolo químico do elemento Oxigênio?',
-      options: ['O', 'O2', 'H2O', 'C'],
-      correctAnswer: 'O',
-      message:'O Oxigênio é representado pelo símbolo "O" na tabela periódica. Este símbolo é usado universalmente para identificar o elemento Oxigênio em reações químicas e fórmulas. Ele não deve ser confundido com "O2", que é a molécula de oxigênio, composta por dois átomos de Oxigênio, como o que respiramos.'
+      question: 'Qual é a menor unidade de um elemento químico que mantém suas propriedades?',
+      options: ['Molécula', 'Átomo', 'Elétron', 'Neutrino'],
+      correctAnswer: 'Átomo',
+      message:''
     },
     {
-      question: 'Qual é o estado físico da água em temperatura ambiente?',
-      options: ['Sólido', 'Líquido', 'Gasoso', 'Plasma'],
-      correctAnswer: 'Líquido',
-      message: 'A água, à temperatura ambiente (cerca de 20 a 25 graus Celsius), encontra-se no estado líquido. Em temperaturas mais baixas, ela pode congelar e se transformar em gelo (sólido), e em temperaturas mais altas, ela pode evaporar e se transformar em vapor (gasoso).'
+      question: 'Qual dos seguintes é um exemplo de uma mistura aquosa?',
+      options: ['Água e óleo', 'Areia e sal', 'Água com sal dissolvido', 'Ferro e enxofre'],
+      correctAnswer: 'Água com sal dissolvido',
+      message: ''
+    },
+    {
+      question: 'Qual das alternativas abaixo representa uma mudança física, e não química?',
+      options: ['Queima de papel', 'Fermentação de uva', 'Dissolução de açúcar em água', 'Oxidação de ferro'],
+      correctAnswer: 'Dissolução de açúcar em água',
+      message: ''
+    },
+    {
+      question: 'Qual partícula subatômica possui carga negativa?',
+      options: ['Próton', 'Nêutron', 'Elétron', 'Quark'],
+      correctAnswer: 'Elétron',
+      message: ''
+    },
+    {
+      question: 'Qual é a unidade de medida da quantidade de matéria no Sistema Internacional (SI)?',
+      options: ['Mol', 'Grama', 'Metro', 'Litro'],
+      correctAnswer: 'Mol',
+      message: ''
+    },
+    {
+      question: 'Qual das alternativas abaixo é um exemplo de uma substância pura?',
+      options: ['Ar', 'Água destilada', 'Água do mar', 'Areia'],
+      correctAnswer: 'Água destilada',
+      message: ''
+    },
+    {
+      question: 'Quantos átomos de hidrogênio estão presentes em uma molécula de água (H₂O)?',
+      options: ['1', '2', '3', '4'],
+      correctAnswer: '2',
+      message: ''
+    },
+    {
+      question: 'Qual das alternativas abaixo representa uma propriedade química?',
+      options: ['Cor', 'Densidade', 'Ponto de fusão', 'Inflamabilidade'],
+      correctAnswer: 'Densidade',
+      message: ''
+    },
+    {
+      question: 'Qual é o principal gás responsável pelo efeito estufa?',
+      options: ['Oxigênio', 'Hidrogênio', 'Gás carbônico', 'Hélio'],
+      correctAnswer: 'Gás carbônico',
+      message: ''
+    },
+    {
+      question: 'Qual das alternativas é uma propriedade dos metais?',
+      options: ['São isolantes térmicos', 'São maleáveis e condutores de eletricidade', 'São quebradiços e isolantes elétricos', 'Têm baixa densidade'],
+      correctAnswer: 'São maleáveis e condutores de eletricidade',
+      message: ''
     }
   ];
 
